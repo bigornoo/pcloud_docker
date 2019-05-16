60a83cb11b7345a4353c244208e00422ef7a67ee
Jean-François Tronche <jftronche@univ-orleans.fr>
2019-05-16 13:03:21 +0200

    rewrite

--- files/run.sh
+++ files/run.sh
@@ -5,34 +5,31 @@
 #Define cleanup procedure
 cleanup() {
     echo "Container stopped, performing cleanup..."
+    fusermount -u /pcloud
 }
 
-if  [ -e /pcloudpassSaved ]; then
-  # subsequent run, pCloud password already saved
-    if ! pgrep -x "fusermount" > /dev/null ; then
-      echo "pCloud not running, starting..."
-      pcloudcc -u $PCLOUDUSER -m /pcloud
-      #Trap SIGTERM
-      trap 'true' SIGTERM
-      #Execute command
-      fusermount -u /pcloud
-      #Wait
-      wait $!
-      #Cleanup
-      cleanup
-    fi
-else
+
+#Trap SIGTERM
+trap 'true' SIGTERM
+#Execute command
+  if  [ -e /pcloudpassSaved ]; then
+      # subsequent run, pCloud password already saved
+      if ! pgrep -x "fusermount" > /dev/null ; then
+          echo "pCloud not running, starting..."
+          pcloudcc -u $PCLOUDUSER -m /pcloud      
+      fi
+  else
     #  first run, save pCloud password
     echo "pCloud initial run, registering password.  You may remove the secret on the next run."
     touch /pcloudpassSaved
     pcloudcc -u $PCLOUDUSER -s -m /pcloud -p </run/secrets/secret1
-    #Trap SIGTERM
-    trap 'true' SIGTERM
-    #Execute command
-    fusermount -u /pcloud
-    #Wait
-    wait $!
-    #Cleanup
-    cleanup
-fi
+  fi
+#Wait
+wait $!
+#Cleanup
+cleanup
+
+
+
+
 
