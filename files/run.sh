62a5d0fc0341d7439ec470b8cd310ee28698e776
Jean-François Tronche <jftronche@univ-orleans.fr>
2019-05-16 14:01:01 +0200

    Retry

--- files/run.sh
+++ files/run.sh
@@ -2,31 +2,43 @@
 # Specials thhanks to James 
 # https://github.com/jamesfkinney/plex-pcloud
 
-#Define cleanup procedure
-cleanup() {
-    echo "Container stopped, performing cleanup..."
+pid=0
+
+# SIGTERM-handler
+term_handler() {
+  if [ $pid -ne 0 ]; then
     fusermount -u /pcloud
+    wait "$pid"
+  fi
+  exit 143; # 128 + 15 -- SIGTERM
 }
 
 
-#Trap SIGTERM
-trap 'cleanup' SIGTERM
-#Execute command
+# setup handlers
+# on callback, kill the last background process, which is `tail -f /dev/null` and execute the specified handler
+trap 'term_handler' SIGTERM
+
+
+
+# Run application
   if  [ -e /pcloudpassSaved ]; then
       # subsequent run, pCloud password already saved
       if ! pgrep -x "fusermount" > /dev/null ; then
           echo "pCloud not running, starting..."
-          pcloudcc -u $PCLOUDUSER -m /pcloud      
+          pcloudcc -u $PCLOUDUSER -m /pcloud -d
       fi
   else
     #  first run, save pCloud password
     echo "pCloud initial run, registering password.  You may remove the secret on the next run."
     touch /pcloudpassSaved
-    pcloudcc -u $PCLOUDUSER -s -m /pcloud -p </run/secrets/secret1
+    pcloudcc -u $PCLOUDUSER -d -s -m /pcloud -p </run/secrets/secret1 
   fi
-#Wait
-wait $(pidof fusermount)
-
+  pid=$(pidof fusermount)  
+# wait forever
+while true
+do
+  tail -f /dev/null & wait $(pid)
+done
 
 
 
